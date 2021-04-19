1f053c7aaa3bdf6d8ba5a12ad2d22564e269a642
EC2 Default User <ec2-user@ip-172-31-63-144.ec2.internal>
2021-04-19 09:00:47 +0000

    [Finish]4-8 課題②

--- bookers1/app/controllers/books_controller.rb
+++ bookers1/app/controllers/books_controller.rb
@@ -8,34 +8,29 @@ class BooksController < ApplicationController
     @book= Book.find(params[:id])
   end
 
-  def new
-  end
-
   def create
-     book = Book.new(book_params)
-    if book.save
+     @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      flash[:notice] = "error! Title and Body can't be blank."
-      redirect_to books_path
+      @books = Book.all
+      render 'index'
     end
   end
-
 
   def edit
    @book= Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
 
-    if book.update(book_params)
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-      flash[:notice] = "error!　Title and Body can't be blank."
-      redirect_to edit_book_path
+      render 'edit'
     end
   end
 

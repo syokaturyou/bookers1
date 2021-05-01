7291b2fd2177c66a9305f2f51700eaa7ac06f176
EC2 Default User <ec2-user@ip-172-31-63-144.ec2.internal>
2021-05-01 12:37:49 +0000

    [Finish]6-1応用課題①

--- bookers1-debug-master/app/controllers/books_controller.rb
+++ bookers1-debug-master/app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new
+    @newbook = Book.new
   end
 
   def create
@@ -13,6 +13,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @newbook=@book
+      @books=Book.all
       render :index
     end
   end
@@ -27,7 +29,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
